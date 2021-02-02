ff794a0fed83833b4dbc070407040ccffa54ff68
matthew croning <matthewcroning@outlook.com>
2021-02-03 07:57:39 +1100

    added searching threads to server

---
+++ forum/src/app/thread/thread-search/thread-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-thread-search',
+  templateUrl: './thread-search.component.html',
+  styleUrls: ['./thread-search.component.scss']
+})
+export class ThreadSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
